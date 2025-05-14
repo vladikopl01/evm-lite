53d033fe9934a9068e19429bf77fe135d83ef716
Vladyslav Kotsiuba <vladyslav.kotsiuba@gmail.com>
2025-05-14 13:09:27 +0200

    feat: ADD, MUL, SUB, PUSHn, DUPn, SWAPn opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,169 @@
 enum Opcode {
-    PUSH1(u8),
-    ADD,
-    STOP,
-    Unknown(u8),
+    STOP,        // 0x00
+    ADD,         // 0x01
+    MUL,         // 0x02
+    SUB,         // 0x03
+    PUSHn(u128), // 0x60 - 0x7F
+    DUPn(u8),    // 0x80 - 0x8F
+    SWAPn(u8),   // 0x90 - 0x9F
+    Unknown(u128),
 }
 
 impl Opcode {
     fn from_byte(codes: &[u8], pc: usize) -> (Self, usize) {
         match codes.get(pc) {
-            Some(0x60) => {
-                let val = *codes.get(pc + 1).unwrap_or(&0);
-                (Opcode::PUSH1(val), 2)
-            }
-            Some(0x01) => (Opcode::ADD, 1),
             Some(0x00) => (Opcode::STOP, 1),
-            Some(&byte) => (Opcode::Unknown(byte), 1),
+            Some(0x01) => (Opcode::ADD, 1),
+            Some(0x02) => (Opcode::MUL, 1),
+            Some(0x03) => (Opcode::SUB, 1),
+            Some(&op) if op >= 0x60 && op <= 0x7F => {
+                let n = (op - 0x60 + 1) as usize;
+                let mut val: u128 = 0;
+                for i in 0..n {
+                    val = (val << 8) | *codes.get(pc + 1 + i).unwrap_or(&0) as u128;
+                }
+                (Opcode::PUSHn(val), n + 1)
+            }
+            Some(&op) if op >= 0x80 && op <= 0x8F => {
+                let n = op - 0x80 + 1;
+                (Opcode::DUPn(n), 1)
+            }
+            Some(&op) if op >= 0x90 && op <= 0x9F => {
+                let n = op - 0x90 + 1;
+                (Opcode::SWAPn(n), 1)
+            }
+            Some(&other) => (Opcode::Unknown(other as u128), 1),
             None => (Opcode::STOP, 1),
         }
     }
 }
 
 struct Evm {
-    stack: Vec<u8>,
+    stack: Vec<u128>,
     pc: usize,
-    codes: Vec<u8>,
+    code: Vec<u8>,
 }
 
 impl Evm {
-    fn new(codes: Vec<u8>) -> Self {
+    fn new(code: Vec<u8>) -> Self {
         Evm {
-            codes,
+            code,
             pc: 0,
             stack: vec![],
         }
     }
 
     fn run(&mut self) {
-        while self.pc < self.codes.len() {
+        while self.pc < self.code.len() {
             self.step();
         }
     }
 
     fn step(&mut self) {
-        let (opcode, consumed_bytes) = Opcode::from_byte(&self.codes, self.pc);
+        let (opcode, consumed_bytes) = Opcode::from_byte(&self.code, self.pc);
         self.pc += consumed_bytes;
 
         match opcode {
-            Opcode::PUSH1(value) => self.stack.push(value),
+            Opcode::STOP => self.pc = self.code.len(),
             Opcode::ADD => {
                 let a = self.stack.pop().expect("Stack underflow");
                 let b = self.stack.pop().expect("Stack underflow");
                 self.stack.push(a + b);
             }
-            Opcode::STOP => self.pc = self.codes.len(),
+            Opcode::MUL => {
+                let a = self.stack.pop().expect("Stack underflow");
+                let b = self.stack.pop().expect("Stack underflow");
+                self.stack.push(a * b);
+            }
+            Opcode::SUB => {
+                let a = self.stack.pop().expect("Stack underflow");
+                let b = self.stack.pop().expect("Stack underflow");
+                self.stack.push(b - a);
+            }
+            Opcode::PUSHn(value) => self.stack.push(value),
+            Opcode::DUPn(n) => {
+                let n = n as usize;
+                if let Some(&value) = self.stack.get(self.stack.len() - n) {
+                    self.stack.push(value);
+                    return;
+                } else {
+                    panic!("Stack underflow");
+                }
+            }
+            Opcode::SWAPn(n) => {
+                let n = n as usize;
+                let len = self.stack.len();
+                if n < len {
+                    self.stack.swap(len - 1, len - n - 1);
+                } else {
+                    panic!("Stack underflow");
+                }
+            }
             Opcode::Unknown(byte) => panic!("Unknown opcode: {:?}", byte),
         }
     }
 }
 
 fn main() {
-    // PUSH1, 0x01, PUSH1, 0x02, ADD, STOP
-    let codes: Vec<u8> = vec![0x60, 0x01, 0x60, 0x02, 0x01, 0x00];
-    let mut evm = Evm::new(codes);
+    let code: Vec<u8> = vec![0x60, 0x01, 0x60, 0x02, 0x70, 0x00];
+    let mut evm = Evm::new(code);
     evm.run();
 
     println!("Final stack: {:?}", evm.stack);
+}
+
+#[test]
+fn test_stop() {
+    let code: Vec<u8> = vec![0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![]);
+}
+
+#[test]
+fn test_add() {
+    let code: Vec<u8> = vec![0x60, 0x01, 0x60, 0x02, 0x01, 0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![3]);
+}
+
+#[test]
+fn test_mul() {
+    let code: Vec<u8> = vec![0x60, 0x02, 0x60, 0x03, 0x02, 0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![6]);
+}
+
+#[test]
+fn test_push() {
+    let code: Vec<u8> = vec![0x60, 0x01, 0x61, 0x01, 0x00, 0x62, 0x01, 0x00, 0x00, 0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![1, 256, 65536]);
+}
+
+#[test]
+fn test_dup() {
+    let code: Vec<u8> = vec![0x60, 0x01, 0x60, 0x02, 0x60, 0x03, 0x81, 0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![1, 2, 3, 2]);
+}
+
+#[test]
+fn test_swap() {
+    let code: Vec<u8> = vec![0x60, 0x01, 0x60, 0x02, 0x60, 0x03, 0x91, 0x00];
+    let mut evm = Evm::new(code);
+    evm.run();
+    assert_eq!(evm.stack, vec![3, 2, 1]);
+}
+
+#[test]
+#[should_panic(expected = "Stack underflow")]
+fn test_add_underwflow() {
+    let code: Vec<u8> = vec![0x01];
+    let mut evm = Evm::new(code);
+    evm.run();
 }
